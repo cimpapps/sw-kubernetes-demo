1d57ad72ac66c104c2515d5881b0e5c6956afec6
catalincimpoeru <catalincimpoeru24@gmail.com>
2020-03-07 21:37:45 +0200

    message

--- src/main/java/com/cimpapps/kubernetesdemo/api/StarWarsDataProvider.java
+++ src/main/java/com/cimpapps/kubernetesdemo/api/StarWarsDataProvider.java
@@ -2,17 +2,16 @@ package com.cimpapps.kubernetesdemo.api;
 
 import com.cimpapps.kubernetesdemo.models.SwPeopleResponse;
 import com.cimpapps.kubernetesdemo.repo.CharacterRepo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.PostConstruct;
 
 @Service
 public class StarWarsDataProvider {
 
-    public static final String SW_API = "https://swapi.co/api/people";
+    @Value("${connector.api.sw.host}")
+    public  String swHost;
     private RestTemplate restTemplate;
     private CharacterRepo characterRepo;
 
@@ -22,7 +21,7 @@ public class StarWarsDataProvider {
     }
 
     public void provideData() {
-        ResponseEntity<SwPeopleResponse> responseEntity = this.restTemplate.getForEntity(SW_API, SwPeopleResponse.class);
+        ResponseEntity<SwPeopleResponse> responseEntity = this.restTemplate.getForEntity(swHost, SwPeopleResponse.class);
         SwPeopleResponse body = responseEntity.getBody();
         characterRepo.saveAll(body.getResults());
     }
